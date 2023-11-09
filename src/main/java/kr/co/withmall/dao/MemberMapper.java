3a5f30e1480935de98840c976ae34d10258be4da
gaeul56 <gaeul56@naver.com>
2023-11-09 12:13:41 +0900

    sge

--- src/main/java/kr/co/withmall/dao/MemberMapper.java
+++
@@ -1,9 +0,0 @@
-package kr.co.withmall.dao;
-
-import kr.co.withmall.dto.MemberDto;
-
-public interface MemberMapper {
-	public int insertMember(MemberDto member);
-	public int insertNaverMember(MemberDto member);
-	
-}
--- src/main/java/kr/co/withmall/util/MyJavaMailUtils.java
+++
@@ -1,5 +0,0 @@
-package kr.co.withmall.util;
-
-public class MyJavaMailUtils {
-
-}
