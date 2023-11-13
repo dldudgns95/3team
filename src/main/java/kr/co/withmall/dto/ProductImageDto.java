18aa232a2b576c8b208011962cfecf5f81c2efe0
eylee02 <leywindy@naver.com>
2023-11-13 10:51:16 +0900

    20231113

---
+++ src/main/java/kr/co/withmall/dto/ProductImageDto.java
@@ -0,0 +1,17 @@
+package kr.co.withmall.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+public class ProductImageDto {
+
+  private String imagePath;
+  private String filesystemName;
+  private ProductDto productDto;  // private int prdtNum;
+}
