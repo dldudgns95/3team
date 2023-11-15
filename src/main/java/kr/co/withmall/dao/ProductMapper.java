593c12e1f14bdd260a2ea68dffbcdc8c775e6821
eylee02 <leywindy@naver.com>
2023-11-15 17:15:30 +0900

    20231115찜하기추가

--- src/main/java/kr/co/withmall/dao/ProductMapper.java
+++ src/main/java/kr/co/withmall/dao/ProductMapper.java
@@ -1,13 +1,18 @@
 package kr.co.withmall.dao;
 
+import java.util.Map;
+
 import org.apache.ibatis.annotations.Mapper;
 
 import kr.co.withmall.dto.ProductDto;
 import kr.co.withmall.dto.ProductImageDto;
+import kr.co.withmall.dto.ZzimDto;
 
 @Mapper
 public interface ProductMapper {
   public ProductDto getProduct(int prdtNum);
   public ProductImageDto getProductImage(int prdtNum);
   public int updateHit(int prdtNum);
+  public int insertZzim(Map<String, Object> map);
+  public ZzimDto getZzimPrdt(Map<String, Object> map);
 }
